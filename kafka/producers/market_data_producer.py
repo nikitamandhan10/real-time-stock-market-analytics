15693e4966c582dd1ca12143cf002b1895b08ccf
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-04-29 18:45:50 -0700

    Update market_data_producer.py

--- kafka/producers/market_data_producer.py
+++ kafka/producers/market_data_producer.py
@@ -14,6 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+
 logger = logging.getLogger(__name__)
 
 class MarketDataProducer:
